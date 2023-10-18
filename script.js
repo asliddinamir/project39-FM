ef1fed7f311717d06199df9426ebc12e5fbd058d
Asliddin Amirov <asliddinamir2@gmail.com>
2023-10-18 14:03:26 +0800

    done responsive the bookmark stats

---
+++ script.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector('.menu');
+const nav = document.querySelector('.nav');
+const overlay = document.querySelector('.overlay');
+
+menu.addEventListener('click', () => {
+    nav.classList.toggle('show-nav');
+    overlay.classList.toggle('show-overlay');
+});
