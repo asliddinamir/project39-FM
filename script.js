402bd72e4b2880b540f63c6f41440e2b88f65ab6
Asliddin Amirov <asliddinamir2@gmail.com>
2023-10-19 11:53:13 +0800

    done bookmarked

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const menu = document.querySelector('.menu');
 const nav = document.querySelector('.nav');
 const overlay = document.querySelector('.overlay');
+const bookmarkBtnDiv = document.querySelector('.bookmarkBtnDiv')
+const bookmarkBtn = document.querySelector('.bookmarkBtn')
 
-menu.addEventListener('click', () => {
+menu.addEventListener('click', toggler)
+overlay.addEventListener('click', toggler)
+
+function toggler() {
     nav.classList.toggle('show-nav');
     overlay.classList.toggle('show-overlay');
+}
+
+bookmarkBtnDiv.addEventListener('click', () => {
+    bookmarkBtnDiv.classList.toggle('bookmarked');
+    if (bookmarkBtnDiv.classList.contains('bookmarked')) {
+        bookmarkBtn.innerText = `Bookmarked`
+    } else {
+        bookmarkBtn.innerText = `Bookmark`
+    }
 });
-
-overlay.addEventListener('click', () => {
-    nav.classList.toggle('show-nav');
-    overlay.classList.toggle('show-overlay');
-})
